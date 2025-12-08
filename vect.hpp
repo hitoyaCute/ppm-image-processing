00416b3be1d806007eaf0cd83d5a16616e40a533
meu <147114029+hitoyaCute@users.noreply.github.com>
2025-12-09 00:08:05 +0800

    Enhance vect with OpenMP and fix data access
    Added OpenMP support and corrected data access in vect structure.

--- vect.hpp
+++ vect.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <omp.h>
 
 /*
 usage
@@ -16,6 +17,7 @@ sin(d);
 #include <array>
 
 template<typename d> struct vect4;
+
 template <typename d, int N>
 struct vect {
     std::array<d,N> data = {0};
@@ -34,6 +36,7 @@ struct vect {
 
     #define op(o) constexpr inline vect<d,N> operator o(const d rhs) const noexcept {\
         vect<d,N> outp;\
+        _Pragma("GCC ivdep")\
         for (int i{0}; i < N; ++i){\
             outp.data[i] = data[i] o rhs;\
         }\
@@ -41,6 +44,7 @@ struct vect {
     }
     #define opf(o) friend constexpr inline vect<d,N> operator o (const d lhs, const vect<d,N>& inp) noexcept{\
         vect<d,N> outp;\
+        _Pragma("GCC ivdep")\
         for (int i{0}; i < N; ++i){\
             outp.data[i] = lhs o inp.data[i];\
         }\
@@ -48,18 +52,21 @@ struct vect {
     }
     #define mop(o) constexpr inline vect<d, N> operator o(const vect<d, N>& rhs) const noexcept {\
         vect<d, N> outp;\
+        _Pragma("GCC ivdep")\
         for (int i{0}; i < N; ++i){\
             outp.data[i] = data[i] o rhs.data[i];\
         }\
         return outp;\
     }
     #define eop(o) constexpr inline vect<d, N>& operator o##=(const d rhs) {\
+        _Pragma("GCC ivdep")\
         for (int i{0}; i< N; ++i) {\
             this->data[i] o##= rhs;\
         }\
         return *this;\
     }
     #define eoop(o) constexpr inline vect<d, N>& operator o##=(const vect<d,N>& rhs) {\
+        _Pragma("GCC ivdep")\
         for (int i{0}; i< N; ++i) {\
             this->data[i] o##= rhs.data[i];\
         }\
@@ -86,13 +93,13 @@ struct vect {
     constexpr inline const d& x() const noexcept {return data[0];}
 
     constexpr inline d& y() noexcept {return data[1];}
-    constexpr inline const d& y() const noexcept {return data[0];}
+    constexpr inline const d& y() const noexcept {return data[1];}
 
-    vect<d,2> xx() const {return vect<d,2>{std::array{data[0],data[0]}};}
-    vect<d,2> yy() const {return vect<d,2>{std::array{data[1],data[1]}};}
-    vect<d,2> yx() const {return vect<d,2>{std::array{data[1],data[0]}};}
-    vect<d,2> xy() const {return vect<d,2>{std::array{data[0],data[1]}};}
-    vect4<d> xyyx() const {return {std::array{data[0],data[1],data[0],data[1]}};}
+    vect<d,2> xx() const {return {std::array{data[0],data[0]}};}
+    vect<d,2> yy() const {return {std::array{data[1],data[1]}};}
+    vect<d,2> yx() const {return {std::array{data[1],data[0]}};}
+    vect<d,2> xy() const {return {std::array{data[0],data[1]}};}
+    vect4<d> xyyx() const {return {data[0],data[1],data[1],data[0]};}
     
 };
 
@@ -100,6 +107,7 @@ struct vect {
 #define mfuncvect(size) template <typename d>\
 constexpr inline d dot(const vect<d,size>& a, const vect<d,size>& b) noexcept {\
     d sum = 0;\
+    _Pragma("GCC ivdep")\
     for (int i = 0; i < size; ++i)\
         sum += a.data[i] * b.data[i];\
     return sum;\
@@ -107,6 +115,7 @@ constexpr inline d dot(const vect<d,size>& a, const vect<d,size>& b) noexcept {\
 template <typename d>\
 constexpr inline vect##size<d> sin(const vect<d,size>& obj) noexcept {\
     vect##size<d> outp;\
+    _Pragma("GCC ivdep")\
     for (int i{0}; i < size; i++) {\
         outp.data[i] = std::sin(obj.data[i]);\
     }\
@@ -115,6 +124,7 @@ constexpr inline vect##size<d> sin(const vect<d,size>& obj) noexcept {\
 template <typename d>\
 constexpr inline vect##size<d> cos(const vect<d,size>& obj) noexcept {\
     vect##size<d> outp;\
+    _Pragma("GCC ivdep")\
     for (int i{0}; i < size; i++) {\
         outp.data[i] = std::cos(obj.data[i]);\
     }\
@@ -123,6 +133,7 @@ constexpr inline vect##size<d> cos(const vect<d,size>& obj) noexcept {\
 template <typename d>\
 constexpr inline vect##size<d> exp(const vect<d,size>& obj) noexcept {\
     vect##size<d> outp;\
+    _Pragma("GCC ivdep")\
     for (int i{0}; i < size; i++) {\
         outp.data[i] = std::exp(obj.data[i]);\
     }\
@@ -131,8 +142,18 @@ constexpr inline vect##size<d> exp(const vect<d,size>& obj) noexcept {\
 template <typename d>\
 constexpr inline vect##size<d> tanh(const vect<d,size>& obj) noexcept {\
     vect##size<d> outp;\
+    _Pragma("GCC ivdep")\
     for (int i{0}; i < size; i++) {\
         outp.data[i] = std::tanh(obj.data[i]);\
+    }\
+    return outp;\
+}\
+template <typename d>\
+constexpr inline vect##size<d> abs(const vect<d,size>& obj) {\
+    vect<d,size> outp;\
+    _Pragma("GCC ivdep")\
+    for (int i{0}; i < size; ++i) {\
+        outp.data[i] = std::abs(obj.data[i]);\
     }\
     return outp;\
 }
@@ -143,7 +164,6 @@ using vect2 = vect<d, 2>;
 
 mfuncvect(2);
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename d>
 struct vect3 : vect<d,3>{
@@ -153,8 +173,8 @@ struct vect3 : vect<d,3>{
 
     constexpr vect3(d x, d y, d z): vect<d, 4>{x,y,z}{}
 
-    constexpr inline d& z() noexcept {return this->data[1];}
-    constexpr inline const d& z() const noexcept {return this->data[0];}
+    constexpr inline d& z() noexcept {return this->data[2];}
+    constexpr inline const d& z() const noexcept {return this->data[2];}
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 mfuncvect(3);
@@ -168,11 +188,11 @@ struct vect4 : vect<d, 4>{
 
     constexpr vect4(d x, d y, d z, d h): vect<d, 4>{x,y,z,h}{}
 
-    constexpr inline d& z() noexcept {return this->data[1];}
-    constexpr inline const d& z() const noexcept {return this->data[0];}
+    constexpr inline d& z() noexcept {return this->data[2];}
+    constexpr inline const d& z() const noexcept {return this->data[2];}
 
     constexpr inline d& h() noexcept {return this->data[3];}
-    constexpr inline const d& h() const noexcept {return this->data[0];}
+    constexpr inline const d& h() const noexcept {return this->data[3];}
 };
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 mfuncvect(4);
@@ -188,5 +208,3 @@ using vec3d = vect3<double>;
 using vec4f = vect4<float>;
 using vec4i = vect4<int>;
 using vec4d = vect4<double>;
-
-
