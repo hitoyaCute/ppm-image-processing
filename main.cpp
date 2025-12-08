cb8785ecd03f80614d6980bc611b094ef5174824
meu <147114029+hitoyaCute@users.noreply.github.com>
2025-12-09 00:07:09 +0800

    Refactor frame_gen function and improve threading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <chrono>
 #include <cmath>
-#include <cstddef>
 #include <thread>
 #include <vector>
 #include <cstdio>
@@ -10,37 +9,33 @@
 #define unpack2(arr2) arr2[0],arr2[1]
 #define area2(arr2) arr2[0] * arr2[1]
 
-
-constexpr void frame_gen(const int i, const int dim[2],
-                         char* buf, const int bufc,
-                         const int vLenght, const int fps,
-                         const char* templ,
-                         char* img_buf, const int img_buf_size) {
+// template <const int img_buf_size>
+inline void frame_gen(FILE* f,
+                      const int i,
+                      const int dim[2],
+                      const int vLenght, const int fps, const int T) {
     // param (const int i, const int dim[2],char buf[256])
-    snprintf(buf, bufc, templ, i);
-    const char* outp_file = buf;
-    FILE *f = std::fopen(outp_file,"wb");
-           
-    std::fprintf(f, "P6\n");
-    std::fprintf(f, "%d %d\n", unpack2(dim));
-    std::fprintf(f, "255\n");
-    
-    const vec2d r{{(float)dim[0],(float)dim[1]}};
+    // char* img_buf = new char(img_buf_size);
 
-    constexpr vec4d u_const{-1.f,1.f,2.f,0.f};
+    
+    const vec2f r{dim[0],dim[1]};
+
+    constexpr vec4f u_const{-1.f,1.f,2.f,0.f};
     const float frames = area2(dim);
 
-    const double t = ((float)i/(fps*4.f))*2.f*M_PI;
-    for (int y{0}; y < dim[1]; ++y) {
-        for (int x{0}; x < dim[0]; ++x) {
+    const float t = ((float)i/(fps*4.f))*2.f*M_PI;
+    for (unsigned int y{0}; y < dim[1]; ++y) {
+        for (unsigned int x{0}; x < dim[0]; ++x) {
 
-            vec4d o{{0.f,0.f,0.f,0.f}};
-            
-            const vec2d FC = {{(float)x,(float)(dim[1] - 1 - y)}};
+            vec4f o;
+            const vec2f FC {dim[0] - 1 - x, dim[1] -1 - y};
 
-            const vec2d p{(FC*2.f-r)/r.y() * ((1.f - (std::abs(frames/2.f - i)/(frames/2.f))) * 10.f +1.f)};
-            // const vec2f p{(FC*2.f-r)/r.y() * 2.f};
-            vec2d l,i,
+            const float scale = 1.f + 5.f * (1 - std::abs(i - frames * 0.5f) / (frames * 0.5f));
+            // const float scale = (float)i/(frames*0.5) * 9.f + 1;
+
+            // const vec2f p{(FC*2.f-r)/r.y()};
+            const vec2f p{((FC*2.f-r)/r.y()) * scale};
+            vec2f l,i,
             v {p*(l += 4.f - 4.f * std::abs(0.7f - dot(p, p)))};
 
             for (; i.y()++ < 8.f; o += (sin(v.xyyx()) + 1.f) * std::abs(v.x() - v.y()))
@@ -48,49 +43,67 @@ constexpr void frame_gen(const int i, const int dim[2],
 
             o = tanh(5.f * exp(l.x() - 4.f - p.y() * u_const) / o);
 
-            // fputc(o.x()*255, f);
-            // fputc(o.y()*255, f);
-            // fputc(o.z()*255, f);
-            const size_t idx = (y * dim[0] + x) * 3;
-            img_buf[idx + 0] = o.x()*255;
-            img_buf[idx + 1] = o.y()*255;
-            img_buf[idx + 2] = o.z()*255;
+            fputc(o.x()*255, f);
+            fputc(o.y()*255, f);
+            fputc(o.z()*255, f);
+            // const size_t idx = (y * dim[0] + x) * 3;
+            // img_buf[idx + 0] = (unsigned char)(o.x()*255.f);
+            // img_buf[idx + 1] = (unsigned char)(o.y()*255.f);
+            // img_buf[idx + 2] = (unsigned char)(o.z()*255.f);
         }
     }
-    fwrite(img_buf, 1, img_buf_size, f);
-    fclose(f);
 }
 
 int main (void){//int argc, char *argv[]) {
     
-    const unsigned T = std::jthread::hardware_concurrency();
-    const int vLenght = 4;
-    const int fps     = 60;
+    const unsigned T = 2;//std::jthread::hardware_concurrency();
+    constexpr int vLenght = 4;
+    constexpr int fps     = 60;
+    constexpr int scalar = 60;
+    constexpr int dim[2] = {16*scalar,9*scalar};
 
+    // tempalte of the output file
     const std::string templa = std::format("output-%0{}d.ppm", (int)std::log10(fps*vLenght) + 1);
+    const char* outp = "output_main.mp4";
+    // i forgot why i did this
+    const char* templ = templa.c_str();
+
     std::printf("ouput template %s\n", templa.c_str());
 
     std::vector<std::jthread> pool;
     pool.reserve(T + 1);
 
-    auto state = std::vector<int>(T);    // state.reserve(T);
+    auto state = std::vector<int>(T);
     std::vector<int> work_ammount{};
     work_ammount.reserve(T);
 
     for (unsigned t = 0; t < T; ++t) {
+
         pool.emplace_back([=,&state,&work_ammount]{
-
+            // for diagnostics
             work_ammount.emplace_back((vLenght*fps)/T);
+            // placeholder for the output template rsults
+            char buf[50];// local for each thread
+            // std::array<unsigned char,  area2(dim) * 3> outp;
 
-            const char* templ = templa.c_str();
-            constexpr int scalar = 60;
-            constexpr int dim[2] = {18*scalar,10*scalar};
-            char buf[50];
+            for (int i = t; i < vLenght*fps; i += T) {
+                // file initialazion
 
-            char img_buf[area2(dim) * 3] {};
-
-            for (unsigned int i = t; i < vLenght*fps; i += T) {
-                frame_gen(i, dim, buf, sizeof(buf), vLenght,fps, templ, img_buf, sizeof(img_buf));
+                snprintf(buf, sizeof(buf), templ, i); // create a file name and put to buf
+                FILE *f = std::fopen(buf,"wb");
+                
+                // header of the file
+                std::fprintf(f, "P6\n");
+                std::fprintf(f, "%d %d\n", unpack2(dim));
+                std::fprintf(f, "255\n");
+                // creating the current frame
+                frame_gen(f,i,dim,vLenght,fps,t);
+                
+                // clearing
+                // fwrite(outp.data(), 1,  area2(dim) * 3, f);
+                fclose(f);
+                
+                // update the status
                 state[t] += 1;
             }
         });
@@ -105,12 +118,12 @@ int main (void){//int argc, char *argv[]) {
                 total += state[i];
 
             vstate = total;
-            std::printf("\x1b[%dA\x1b[2K\rProgress: %7.3f%% with worker count %d\n", T+3, (float)vstate / (vLenght * 60) * 100, T);
+            std::printf("\x1b[%dA\x1b[2K\rProgress: %7.3f%% with worker count %d\n", T+3, (float)vstate / (vLenght * fps) * 100, T);
             std::printf("   workers:\n");
             for (unsigned worker{0}; worker < T; worker ++) {
                 std::printf("      worker_%02d: job %4d/%4d\n", worker, state[worker], work_ammount[worker]);
             }
-            std::printf("\n      total work %4d/%4d", vstate, fps*vLenght);
+            std::printf("\n      total work %4d/%4d   ", vstate, fps*vLenght);
             std::fflush(stdout);
 
             std::this_thread::sleep_for(std::chrono::milliseconds(250));
@@ -122,7 +135,7 @@ int main (void){//int argc, char *argv[]) {
     std::printf("Finished creating %d images\n", fps*vLenght);
     std::fflush(stdout);
 
-    auto d = system(std::format("ffmpeg -y -r {} -i {} output.mp4 > /dev/null 2>&1", fps, templa).c_str());
+    auto d = system(std::format("ffmpeg -y -r {} -i {} {} > /dev/null 2>&1", fps, templa, outp).c_str());
 
     std::printf("Finished cimpiling into video deleting images\n");
     std::fflush(stdout);
@@ -130,39 +143,3 @@ int main (void){//int argc, char *argv[]) {
     (void)system("rm output-*.ppm");
     return 0;
 }
-
-    // std::vector<std::jthread> pool;
-    //
-    // for (int i{0}; i<60; ++i)
-    //     pool.emplace_back([&, i]{
-    //         const int scalar = 60;
-    //         const int dim[2] = {16*scalar,9*scalar};
-    //         frame_gen(i, dim);
-    //     });
-    // }
-    
-    // for (int i{0}; i<240; i++) {
-    //     // std::printf("file '%s' has been created\n", outp_file);
-    //     snprintf(buf, sizeof(buf), "output-%04d.ppm",i);
-    //     const char* outp_file = buf;
-    //     FILE *f = std::fopen(outp_file,"wb");
-    //
-    //     std::fprintf(f, "P6\n");
-    //     std::fprintf(f, "%d %d\n", unpack2(dim));
-    //     std::fprintf(f, "255\n");
-    //     for (int y{0}; y < dim[1]; y++) {
-    //         for (int x{0}; x < dim[0]; x++) {
-    //             if (((x+i)/60 + (y+i)/60)%2) {
-    //                 fputc(0xff, f);
-    //                 fputc(0x00, f);
-    //                 fputc(0x00, f);
-    //             } else {
-    //                 fputc(0x00, f);
-    //                 fputc(0x00, f);
-    //                 fputc(0x00, f);
-    //             }
-    //         }
-    //     }
-    //     fclose(f);
-    //
-    // }
