60753395ae06c3e4fa8deedce36c77f2f6c6b312
meu <147114029+hitoyaCute@users.noreply.github.com>
2025-12-09 22:12:10 +0800

    Enhance signal handling and threading in main.cpp
    Refactor main.cpp to improve signal handling and threading.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include <atomic>
 #include <chrono>
 #include <cmath>
+#include <csignal>
 #include <thread>
 #include <vector>
 #include <cstdio>
@@ -9,32 +11,32 @@
 #define unpack2(arr2) arr2[0],arr2[1]
 #define area2(arr2) arr2[0] * arr2[1]
 
+std::atomic<bool> interupted{false};
+
+void handle_sig(const int sig) {
+    interupted = true;
+}
+
 // template <const int img_buf_size>
 inline void frame_gen(FILE* f,
                       const int i,
                       const int dim[2],
                       const int vLenght, const int fps, const int T) {
     // param (const int i, const int dim[2],char buf[256])
-    // char* img_buf = new char(img_buf_size);
-
-    
+    // char* img_buf = new char(img_buf_size);    
     const vec2f r{dim[0],dim[1]};
 
     constexpr vec4f u_const{-1.f,1.f,2.f,0.f};
-    const float frames = area2(dim);
+    const float frames = fps * vLenght;
 
-    const float t = ((float)i/(fps*4.f))*2.f*M_PI;
+    const float t = ((float)i/(frames))*2.f*M_PI;
     for (unsigned int y{0}; y < dim[1]; ++y) {
         for (unsigned int x{0}; x < dim[0]; ++x) {
 
             vec4f o;
             const vec2f FC {dim[0] - 1 - x, dim[1] -1 - y};
 
-            const float scale = 1.f + 5.f * (1 - std::abs(i - frames * 0.5f) / (frames * 0.5f));
-            // const float scale = (float)i/(frames*0.5) * 9.f + 1;
-
-            // const vec2f p{(FC*2.f-r)/r.y()};
-            const vec2f p{((FC*2.f-r)/r.y()) * scale};
+            const vec2f p{((FC*2.f-r)/r.y()) * 1.3};
             vec2f l,i,
             v {p*(l += 4.f - 4.f * std::abs(0.7f - dot(p, p)))};
 
@@ -54,17 +56,18 @@ inline void frame_gen(FILE* f,
     }
 }
 
-int main (void){//int argc, char *argv[]) {
-    
-    const unsigned T = 2;//std::jthread::hardware_concurrency();
+int main (void){//int argc, char *argv[]) {  
+    std::signal(SIGINT, [](int sig) -> void {handle_sig(sig);});
+
+    const unsigned T = std::jthread::hardware_concurrency();
     constexpr int vLenght = 4;
-    constexpr int fps     = 60;
-    constexpr int scalar = 60;
+    constexpr int fps     = 15;
+    constexpr int scalar = 90;
     constexpr int dim[2] = {16*scalar,9*scalar};
 
     // tempalte of the output file
     const std::string templa = std::format("output-%0{}d.ppm", (int)std::log10(fps*vLenght) + 1);
-    const char* outp = "output_main.mp4";
+    const char* outp = "output_main.gif";
     // i forgot why i did this
     const char* templ = templa.c_str();
 
@@ -79,7 +82,7 @@ int main (void){//int argc, char *argv[]) {
 
     for (unsigned t = 0; t < T; ++t) {
 
-        pool.emplace_back([=,&state,&work_ammount]{
+        pool.emplace_back([=,&state,&work_ammount](const std::stop_token st){
             // for diagnostics
             work_ammount.emplace_back((vLenght*fps)/T);
             // placeholder for the output template rsults
@@ -88,6 +91,9 @@ int main (void){//int argc, char *argv[]) {
 
             for (int i = t; i < vLenght*fps; i += T) {
                 // file initialazion
+                if (st.stop_requested()) {
+                    break;
+                }
 
                 snprintf(buf, sizeof(buf), templ, i); // create a file name and put to buf
                 FILE *f = std::fopen(buf,"wb");
@@ -108,11 +114,11 @@ int main (void){//int argc, char *argv[]) {
             }
         });
     }
-    pool.emplace_back([=,&state,&work_ammount]{
+    pool.emplace_back([=,&state,&work_ammount](std::stop_token ts){
         int vstate = 0;
         for (unsigned i{0}; i < T+3; i++)
             std::printf("\x1b[2K\n");
-        while (vstate < (vLenght * fps)) {
+        while ((vstate < (vLenght * fps)) and !ts.stop_requested()) {
             int total = 0;
             for (int i{0}; i < T; i++)
                 total += state[i];
@@ -128,10 +134,39 @@ int main (void){//int argc, char *argv[]) {
 
             std::this_thread::sleep_for(std::chrono::milliseconds(250));
         }
-        std::printf("\nDone!\n"); 
+        std::printf("\nDone!\n");
     });
+
+    while (true) {
+        bool running = true;
+        for (int i{0}; i<pool.size(); i++) {
+            running = pool[i].joinable();
+        }
+        if (interupted) {
+            for (int i{0}; i<pool.size(); i++) {
+                pool[i].request_stop();
+                pool[i].join();
+            }
+            running = false;
+        }
+        if (not running) break;
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    }
+
     pool.clear();
     state.clear();
+
+    if (interupted) {
+        std::printf("\n\n\n\n\n\nkilled exiting save temp\?\?(Y/n):");
+        char res = fgetc(stdin);
+        if (res == 'N' or res == 'n') {
+            system("rm *.ppm");
+            std::printf("files deleted\n");
+        } else {
+            std::printf("files saved exiting\n");
+        }
+        exit(0);
+    }
     std::printf("Finished creating %d images\n", fps*vLenght);
     std::fflush(stdout);
 
